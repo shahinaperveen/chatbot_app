49bc310b343b206bbe288e78dbd14bc052053c18
shahinaperveen <148042755+shahinaperveen@users.noreply.github.com>
2023-10-24 23:18:55 +0500

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from streamlit_chat import message
 from langchain.callbacks import get_openai_callback
 from sentence_transformers import SentenceTransformer
 
-openapi_key = st.secrets["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"]
+openapi_key = st.secrets["open_ai_key"]
 
 # "with" notation
 def main():
